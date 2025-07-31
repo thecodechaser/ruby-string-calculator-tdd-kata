a8d1b5eac0e04d5c4642259dc60440c66fc39b34
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-07-31 17:03:23 +0530

    Add code for two comma seperated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,9 @@ class StringCalculator
 
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    return numbers.to_i if !numbers.include?(',')
+
+    numbers.split(",").map(&:to_i).sum
   end
 
 end
