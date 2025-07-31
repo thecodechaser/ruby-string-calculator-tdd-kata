3e2aa5b6be9499509ffc9d60d7ca3fa99721f0d8
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-07-31 17:14:52 +0530

    Add test for all delimiters

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -35,6 +35,17 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("2,1001")).to eq(2)
     end
 
+    it 'supports delimiters of any length' do
+      expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'supports delimiters of any length' do
+      expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'supports multiple multi-character delimiters' do
+      expect(StringCalculator.add("//[**][%%]\n1**2%%3")).to eq(6)
+    end
 
   end
 end
