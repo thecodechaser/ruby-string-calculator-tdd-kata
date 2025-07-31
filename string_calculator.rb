6d8c332fba1e5ac6914df6f39f0d21b5c32e8652
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-07-31 17:06:51 +0530

    Add code fir newlines as delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,9 +2,7 @@ class StringCalculator
 
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if !numbers.include?(',')
-
-    numbers.split(",").map(&:to_i).sum
+    numbers.split(/[\n,]/).map(&to_i).sum
   end
 
 end
